4130f5fada5c336abbfc99b54c4b99e0682f1714
Zeed <2996950868@qq.com>
2023-12-26 17:13:20 +0800

    "new"

--- GFPGAN-master/inference_gfpgan.py
+++ GFPGAN-master/inference_gfpgan.py
@@ -113,7 +113,7 @@ def main():
     for img_path in tqdm(img_list):
         # read image
         img_name = os.path.basename(img_path)
-        print(f'Processing {img_name} ...')
+        # print(f'Processing {img_name} ...')
         basename, ext = os.path.splitext(img_name)
         input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
